4961d61b3c1603f452770d2d705ccd2d61952080
iMac <huangxiujie@163.com>
2017-10-30 17:19:48 +0800

    appid

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 
 // 初始化AV
 const AV = require('./utils/av-weapp.js');
-const appId = "7tm1OFlNlmLFukegUhmm4uDU-gzGzoHsz";
-const appKey = "XG4FRumQWJ7mNkFIral0ttvj";
+const appId = "SgHcsYqoLaFTG0XDMD3Gtm0I-gzGzoHsz";
+const appKey = "xdv2nwjUK5waNglFoFXkQcxP";
 
 AV.init({ 
 	appId: appId, 
